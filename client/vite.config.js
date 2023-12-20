e83b2118903f1680abe6e99ad4cb1f78406b7030
Vijay Kumar T <vjkumar7293@gmail.com>
2023-12-21 01:19:42 +0530

    removed vite config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3001,
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -17,10 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
-  },
-  build: {
-    rollupOptions: {
-      external: ["path/to/hljs.js"],
-    },
   },
 });
