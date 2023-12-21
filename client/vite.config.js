0dd53a448c5eb82b355b895057debf10dd8b2988
Vijay Kumar T <vjkumar7293@gmail.com>
2023-12-21 08:03:20 +0530

    deleted vite.svg in public folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,14 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3001,
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,5 +17,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+  },
+  build: {
+    rollupOptions: {
+      external: ["path/to/go.min.js"],
+    },
   },
 });
