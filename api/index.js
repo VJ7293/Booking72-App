6d7e4891a8356e006e7634610c857276d1f7e0e7
Vijay Kumar T <vjkumar7293@gmail.com>
2024-01-22 11:10:21 +0530

    Add node_modules and .env to .gitignore

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
 // Import necessary modules
 const express = require("express");
-
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/user");
 const Place = require("./models/place");
 const Booking = require("./models/booking");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const imageDownloader = require("image-downloader");
 const multer = require("multer");
 const fs = require("fs");
@@ -20,17 +19,9 @@ const app = express();
 // Set up middleware and configurations
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = "tyasuyfiusdvlkdfnblkfsdnfbkjn";
-
-const corsOptions = {
-  credentials: true,
-  origin: "http://localhost:3001",
-};
-
-// Use cors middleware
-// app.use(cors(corsOptions));
-app.use(cors(corsOptions));
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
+
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
@@ -39,25 +30,32 @@ app.use(
   })
 );
 
-app.use(
-  "/src",
-  express.static("src", {
-    setHeaders: (res, path) => {
-      if (path.endsWith(".jsx")) {
-        res.setHeader("Content-Type", "text/javascript");
-      }
-    },
-  })
-);
-
-// Middleware to handle .mjs files
-app.get("*.mjs", (req, res, next) => {
-  res.type("application/javascript");
-  next();
-});
+// Now you can use the userData, which may contain the decoded user information
 
 // Function to extract user data from JWT token
+// function getUserDataFromReq(req) {
+//   mongoose.connect(process.env.MONGO_URL);
+//   return new Promise((resolve, reject) => {
+//     const token = req.cookies && req.cookies.token;
+
+//     console.log("Received token:", token);
+
+//     if (!token) {
+//       reject(jwt.JsonWebTokenError("jwt must be provided"));
+//       return;
+//     }
+
+//     jwt.verify(token, jwtSecret, {}, (err, userData) => {
+//       if (err) {
+//         reject(new jwt.JsonWebTokenError("Invalid token"));
+//       } else {
+//         resolve(userData);
+//       }
+//     });
+//   });
+// }
 function getUserDataFromReq(req) {
+  mongoose.connect(process.env.MONGO_URL);
   return new Promise((resolve, reject) => {
     jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
       if (err) throw err;
@@ -66,23 +64,15 @@ function getUserDataFromReq(req) {
   });
 }
 
-// Connect to MongoDB
-(async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-  }
-})();
-
 // Define a test endpoint
 app.get("/test", (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
   res.json({ message: "test ok" });
 });
 
 // User registration endpoint
 app.post("/register", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
   // Extract user registration data from the request body
   const { name, email, password } = req.body;
 
@@ -97,63 +87,53 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch (error) {
     // Handle registration failure
+    console.error("User registration failed:", error);
     res.status(422).json({ error: "User registration failed" });
   }
 });
 
 // User login endpoint
 app.post("/login", async (req, res) => {
-  // Extract login credentials from the request body
+  mongoose.connect(process.env.MONGO_URL);
   const { email, password } = req.body;
-  // Find a user document with the provided email in the MongoDB User collection
   const userDoc = await User.findOne({ email });
   if (userDoc) {
-    // Check if the provided password matches the hashed password in the database
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
-      // If passwords match, create a JWT token and set it as a cookie
       jwt.sign(
-        { email: userDoc.email, id: userDoc._id, name: userDoc.name },
+        {
+          email: userDoc.email,
+          id: userDoc._id,
+        },
         jwtSecret,
         {},
         (err, token) => {
           if (err) throw err;
-          res.cookie("token", token, {}).json(userDoc);
+          res.cookie("token", token).json(userDoc);
         }
       );
     } else {
-      // Respond with an error if passwords do not match
-      res.status(422).json("Password not correct");
+      res.status(422).json("pass not ok");
     }
   } else {
-    // Respond with an error if the user with the provided email is not found
-    res.status(404).json("User not found");
+    res.json("not found");
   }
 });
 
 // Endpoint to get user profile information
-app.get("/profile", (req, res) => {
-  // Extract JWT token from cookies
-  const { token } = req.cookies;
-  if (token) {
-    // Verify the token and fetch user data from MongoDB
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-      if (err) {
-        return res.status(401).json({ error: "JWT verification failed" });
-      }
+app.get("/profile", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
 
-      try {
-        const { name, email, _id } = await User.findById(userData.id);
-        // Respond with user profile information
-        res.json({ name, email, _id });
-      } catch (error) {
-        // Handle error fetching user data
-        return res.status(500).json({ error: "Error fetching user data" });
-      }
-    });
-  } else {
-    // Respond with an error if the token is not found
-    res.status(401).json({ error: "Token not found" });
+  try {
+    const userData = await getUserDataFromReq(req);
+
+    const { name, email, _id } = await User.findById(userData.id);
+    // Respond with user profile information
+    res.json({ name, email, _id });
+  } catch (error) {
+    // Handle error fetching user data
+    console.error("Error fetching user data:", error);
+    return res.status(500).json({ error: "Error fetching user data" });
   }
 });
 
@@ -167,13 +147,19 @@ app.post("/logout", (req, res) => {
 app.post("/upload-by-link", async (req, res) => {
   const { link } = req.body;
   const newName = "photo" + Date.now() + ".jpg";
-  // Download and save the image to the 'uploads' directory
-  await imageDownloader.image({
-    url: link,
-    dest: __dirname + "/uploads/" + newName,
-  });
-  // Respond with the new image filename
-  res.json(newName);
+
+  try {
+    // Download and save the image to the 'uploads' directory
+    await imageDownloader.image({
+      url: link,
+      dest: __dirname + "/uploads/" + newName,
+    });
+    // Respond with the new image filename
+    res.json(newName);
+  } catch (error) {
+    console.error("Image download error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 // Endpoint to upload images from the local device
@@ -202,7 +188,7 @@ app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
 });
 
 // Endpoint to create a new place
-app.post("/places", (req, res) => {
+app.post("/places", async (req, res) => {
   // Extract data from the request body
   const { token } = req.cookies;
   const {
@@ -221,51 +207,84 @@ app.post("/places", (req, res) => {
   // Verify the JWT token
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     if (err) {
+      console.error("JWT verification failed:", err);
       return res.status(401).json({ error: "Unauthorized" });
     }
 
-    // Create a new place document in the MongoDB Place collection
-    const placeDoc = await Place.create({
-      owner: userData.id,
-      price,
-      title,
-      address,
-      photos: addedPhotos,
-      description,
-      perks,
-      extraInfo,
-      checkIn,
-      checkOut,
-      maxGuest,
-    });
+    try {
+      // Create a new place document in the MongoDB Place collection
+      const placeDoc = await Place.create({
+        owner: userData.id,
+        price,
+        title,
+        address,
+        photos: addedPhotos,
+        description,
+        perks,
+        extraInfo,
+        checkIn,
+        checkOut,
+        maxGuest,
+      });
 
-    // Respond with the created place document
-    res.json(placeDoc);
+      // Respond with the created place document
+      res.json(placeDoc);
+    } catch (error) {
+      console.error("Create place error:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   });
 });
 
 // Endpoint to get places owned by a user
-app.get("/user-places", (req, res) => {
-  // Extract data from the request
-  const { token } = req.cookies;
+// app.get("/user-places", (req, res) => {
+//   // Extract data from the request
+//   mongoose.connect(process.env.MONGO_URL);
+//   const { token } = req.cookies;
 
-  // Verify the JWT token
-  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+//   // Verify the JWT token
+//   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+//     // Fetch places owned by the user from MongoDB
+//     const { id } = userData;
+//     res.json(await Place.find({ owner: id }));
+//   });
+// });
+app.get("/user-places", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  try {
+    const userData = await getUserDataFromReq(req);
+
     // Fetch places owned by the user from MongoDB
-    const { id } = userData;
-    res.json(await Place.find({ owner: id }));
-  });
+    const userPlaces = await Place.find({ owner: userData.id });
+    res.json(userPlaces);
+  } catch (error) {
+    console.error("User places fetch error:", error);
+    res.status(401).json({ error: "Unauthorized" });
+  }
 });
 
 // Endpoint to get details of a specific place by ID
 app.get("/places/:id", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
   const { id } = req.params;
-  // Fetch the place document from MongoDB by ID
   res.json(await Place.findById(id));
 });
+// app.get("/places/:id", async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     // Fetch the place document from MongoDB by ID
+//     const placeDetails = await Place.findById(id);
+//     res.json(placeDetails);
+//   } catch (error) {
+//     console.error("Fetch place details error:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
 // Endpoint to update place details
 app.put("/places", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
   // Extract data from the request
   const { token } = req.cookies;
   const {
@@ -284,77 +303,117 @@ app.put("/places", async (req, res) => {
 
   // Verify the JWT token
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-    if (err) throw err;
+    if (err) {
+      console.error("JWT verification failed:", err);
+      return res.status(401).json({ error: "Unauthorized" });
+    }
 
-    // Fetch the place document from MongoDB by ID
-    const placeDoc = await Place.findById(id);
+    try {
+      // Fetch the place document from MongoDB by ID
+      const placeDoc = await Place.findById(id);
 
-    // Check if the user is the owner of the place
-    if (userData.id === placeDoc.owner.toString()) {
-      // Update the place document with the new details
-      placeDoc.set({
-        title,
-        address,
-        photos: addedPhotos,
-        description,
-        perks,
-        extraInfo,
-        checkIn,
-        checkOut,
-        maxGuest,
-        price,
-      });
-      // Save the updated place document
-      await placeDoc.save();
-      // Respond with success
-      res.json("ok");
+      // Check if the user is the owner of the place and userData is defined
+      if (userData && userData.id === placeDoc.owner.toString()) {
+        // Update the place document with the new details
+        placeDoc.set({
+          title,
+          address,
+          photos: addedPhotos,
+          description,
+          perks,
+          extraInfo,
+          checkIn,
+          checkOut,
+          maxGuest,
+          price,
+        });
+        // Save the updated place document
+        await placeDoc.save();
+        // Respond with success
+        res.json("ok");
+      } else {
+        res
+          .status(403)
+          .json({ error: "Forbidden: You are not the owner of this place" });
+      }
+    } catch (error) {
+      console.error("Update place error:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
   });
 });
 
 // Endpoint to get a list of all places
 app.get("/places", async (req, res) => {
-  // Fetch all places from MongoDB
+  mongoose.connect(process.env.MONGO_URL);
   res.json(await Place.find());
 });
+// app.get("/places", async (req, res) => {
+//   try {
+//     // Fetch all places from MongoDB
+//     const allPlaces = await Place.find();
+//     res.json(allPlaces);
+//   } catch (error) {
+//     console.error("Fetch all places error:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
 // Endpoint to create a new booking
 app.post("/bookings", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
   // Extract data from the request
   const userData = await getUserDataFromReq(req);
   const { place, checkIn, checkOut, numberOfGuests, name, phone, price } =
     req.body;
 
-  // Create a new booking document in the MongoDB Booking collection
-  Booking.create({
-    place,
-    checkIn,
-    checkOut,
-    numberOfGuests,
-    name,
-    phone,
-    price,
-    user: userData.id,
-  })
-    .then((doc) => {
-      // Respond with the created booking document
-      res.json(doc);
-    })
-    .catch((err) => {
-      throw err;
+  try {
+    // Create a new booking document in the MongoDB Booking collection
+    const bookingDoc = await Booking.create({
+      place,
+      checkIn,
+      checkOut,
+      numberOfGuests,
+      name,
+      phone,
+      price,
+      user: userData.id,
     });
+    // Respond with the created booking document
+    res.json(bookingDoc);
+  } catch (error) {
+    console.error("Create booking error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 // Endpoint to get a list of bookings for the authenticated user
 app.get("/bookings", async (req, res) => {
-  // Extract user data from the request
+  mongoose.connect(process.env.MONGO_URL);
   const userData = await getUserDataFromReq(req);
-  // Fetch bookings for the user from MongoDB, populating the associated place information
   res.json(await Booking.find({ user: userData.id }).populate("place"));
 });
+// app.get("/bookings", async (req, res) => {
+//   // Extract user data from the request
+//   const userData = await getUserDataFromReq(req);
+
+//   try {
+//     // Fetch bookings for the user from MongoDB, populating the associated place information
+//     const userBookings = await Booking.find({ user: userData.id }).populate(
+//       "place"
+//     );
+//     res.json(userBookings);
+//   } catch (error) {
+//     console.error("Fetch user bookings error:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
 // Start the Express app on port 4000
-// app.listen(4000);
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
